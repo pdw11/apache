49ae6a1e3099876ec49ba8fa5b9101a99aab9339
pdw11 <33090791+pdw11@users.noreply.github.com>
2018-03-05 22:32:38 +0000

    first init file

--- apache/manifests/init.pp
+++ apache/manifests/init.pp
@@ -44,5 +44,38 @@
 #
 class apache {
 
+ case $operatingsystem {
 
+	centos: {
+	  $webserver= 'httpd'
+	  $confpath= "/etc/$webserver/conf/$webserver.conf"
+ }
+
+	ubuntu: {
+	  $webserver= 'apache2'
+	  $confpath= "/etc/$webserver/$webserver.conf"
+ }
+
+	default: {
+	  fail ("Unsupported OS so it is")
+ }	
+
+}
+
+package { 'apache':
+       name   => $webserver,
+       ensure => installed,
+}
+file {'apacheconf':
+       name    => $confpath,
+       ensure  => file,
+       mode    => 600,
+       source  => "puppet:///modules/apache/$webserver.conf",
+       require => Package['apache'],
+}
+service {'apache':
+          name   => $webserver,
+          ensure   => running,
+          enable   => true,
+          subscribe => File['apacheconf'],
 }
